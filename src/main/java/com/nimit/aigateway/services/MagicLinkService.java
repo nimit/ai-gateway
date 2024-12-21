1f9f43c8f28db51c5e8978cc409bbb6cbeccc607
Nimit <nimit.shah622@gmail.com>
2024-12-21 04:17:57 +0000

    minor

--- src/main/java/com/nimit/aigateway/services/MagicLinkService.java
+++ src/main/java/com/nimit/aigateway/services/MagicLinkService.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
